9c0b30739ae43e0cb51615467573ff5acdbeed5a
Rakesh9254 <rakeshkumarroshan62@gmail.com>
2025-12-21 19:32:58 +0530

    Add Hello.py Python script

---
+++ Hello.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("Hello, Rakesh! Python is working 🚀")
+# written by Rakesh
+print("Hello \nWorld!")
+price = 10
+tax = 20
+total_amount = price + tax
+print(total_amount)
