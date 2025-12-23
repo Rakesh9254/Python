798dea203064bf12895440cbf700448669ffa0fb
Rakesh9254 <rakeshkumarroshan62@gmail.com>
2025-12-23 20:28:28 +0530

    Taking input from keyboard

---
+++ input.py
@@ -0,0 +1,20 @@
+name =input("Enter the name: ")
+print(name)
+print(type(name))
+
+age =input("Enter the age: ")
+print(age)
+print(type(age))
+
+
+
+age: str =input("Enter the age: ")
+print(age)
+print(type(age))
+
+
+
+
+Mobile =int(input("Enter your Mobile No: "))
+print(Mobile)
+print(type(Mobile))
