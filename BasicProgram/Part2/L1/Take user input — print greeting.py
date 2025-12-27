5a453aed28fb8db27e0d0e0aac73b28d6c5ed288
Rakesh9254 <rakeshkumarroshan62@gmail.com>
2025-12-27 17:04:55 +0530

    Today Assissgnments

---
+++ BasicProgram/Part2/L1/Take user input — print greeting.py
@@ -0,0 +1,18 @@
+# 1) Greeting Program
+name = input("Enter your name: ")
+print("Hello", name)
+
+
+# 2) Sum of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
+
+
+# 3) Age check
+age = int(input("Enter age: "))
+
+if age < 18:
+    print("Minor")
+else:
+    print("Adult")
