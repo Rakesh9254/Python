e82cfe0b865516779ae569380af0ac89ebb6d571
Rakesh9254 <rakeshkumarroshan62@gmail.com>
2026-01-18 10:24:11 +0530

    Multiple quotes

--- ImpProgram/String/multiple.py
+++ ImpProgram/String/multiple.py
@@ -4,3 +4,24 @@ text="This is Rahul's book"
 # text ='This is Rahul's book' --------
 print(text)
 #Use double quotes outside, single quote inside.
+
+print("Hello\nWorld") # new line
+print("Hello\tWorld") # tab space
+
+
+str1="Apna"
+str2="College"
+print(str1+" "+str2)
+
+
+# Length of a String – len()
+str ="Apna Bihar"
+print(len(str))
+
+
+s = "python"
+print(s.capitalize())
+
+
+s = "Python"
+print(s.find("m"))  # index
